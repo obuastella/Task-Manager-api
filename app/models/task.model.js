b20a92eeab23b54cc098207bf18dd85a6738f4fe
obuastella <stellaobua123@gmail.com>
2026-03-11 02:11:19 +0100

    db-model

---
+++ app/models/task.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      minLength: 5,
+      maxLength: 50,
+      trim: true,
+    },
+    status: {
+      enum: ["pending", "completed"],
+      default: "pending",
+    },
+  },
+  { timestamps: true },
+);
+const Task = mongoose.model("Task", taskSchema);
+
+export default Task;
